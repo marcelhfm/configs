06ce6836f5f12227cfdb538fcab614ac31b0c70d
Marcel Hofmann <marcel.hofmann@deepimmo.com>
2025-03-27 12:43:01 +0100

    feat: test onedark theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1164,13 +1164,22 @@ require('lazy').setup({
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
-    'rebelot/kanagawa.nvim',
+    --'rebelot/kanagawa.nvim',
+    'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      require('onedark').setup {
+        style = 'warmer',
+      }
+    end,
+
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      vim.cmd.colorscheme 'onedark'
+
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
